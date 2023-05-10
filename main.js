3ef6529926782c1fa163ee784369342f2afb4e34
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-05-10 16:23:58 -0500

    example while

---
+++ main.js
@@ -0,0 +1,26 @@
+// for
+
+
+// for anidado
+
+
+
+
+
+// while
+let x = 0;
+while (x < 10) {
+    alert ("Ahora x vale " + x);
+    console.log ("Ciclo numero " + x);
+    x = x + 1;
+}
+
+
+
+
+// do..while
+
+
+
+
+// switch
