4f75f8a6cd06a5c74dcdcae4fb5aab8372d2a724
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-05-10 17:15:13 -0500

    do...while

--- main.js
+++ main.js
@@ -5,15 +5,15 @@
 // }
 
 
-// for anidado
+// // for anidado
 
-for (let x = 0 ; x < 10; x ++) {
-    console.log ("Ciclo numero " + x);
+// for (let x = 0 ; x < 10; x ++) {
+//     console.log ("Ciclo numero " + x);
 
-    for (let y =0; y < 5 ; y++) {
-        console.log (" Ciclo secundario " + y);
-    }
-}
+//     for (let y =0; y < 5 ; y++) {
+//         console.log (" Ciclo secundario " + y);
+//     }
+// }
 
 
 
@@ -31,7 +31,11 @@ for (let x = 0 ; x < 10; x ++) {
 
 // do..while
 
-
+let z = 0;
+do {
+    console.log ("Ciclo do..while numero " + z);
+    z++;
+} while (z < 5);
 
 
 // switch
